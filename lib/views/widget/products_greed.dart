1305d52e32b75e60bcf4aba3313289d256564dd4
Denis Kotenko <d.kotenko@rj-games.com>
2021-11-30 00:31:35 +0300

    refactoring provider builder

--- lib/views/widget/products_greed.dart
+++ lib/views/widget/products_greed.dart
@@ -13,8 +13,8 @@ class ProductsGreed extends StatelessWidget {
     return GridView.builder(
       itemCount: products.length,
       padding: const EdgeInsets.all(10),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
         child: ProductItem(),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
