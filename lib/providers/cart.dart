148b05c3deffc8fc1bbcf2c554ccdcfdd3b11247
Denis Kotenko <d.kotenko@rj-games.com>
2021-11-30 22:09:44 +0300

    cart provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'cart_item.dart';
+
+class Cart with ChangeNotifier{
+  final Map<String,CartItem> _items = {};
+
+
+}
---
+++ lib/providers/cart_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CartItem with ChangeNotifier {
+  final String id;
+  late int quantity = 0;
+
+  CartItem(this.id);
+}
